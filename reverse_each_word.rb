e6b66282fef65bb5f2ccffc19e94ee8eecedf285
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-06 21:06:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@ def reverse_each_word(string)
   string = "Hello there, and how are you?"
   string.reverse
   string.split
-
-
-
-end
-#str = "a,b,c"
-#list = str.split(/,/) # => ["a", "b", "c"]
+  end
+  # returns that same sentence with each word reversed in place.
+    #First solve it using .each Then write the same method using .collect to see the difference.
+    #turn the string into an array
+    #.reverse reverses an array
